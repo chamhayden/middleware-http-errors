c804bab822a3d01d4e9bf90eb8f3857e61eb3996
Khiet Tam Nguyen <nktnet999@gmail.com>
2023-05-08 17:22:37 +1000

    Fixed { error: 'some message' } instead of { error: { message: 'some message' } }' and removed opts to avoid tsc failures

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
-const errorHandler = (opts = {}) => (err: any, req: Request, res: Response, next: NextFunction) => {
+const errorHandler = () => (err: any, req: Request, res: Response, next: NextFunction) => {
   const statusCode = err.status || err.statusCode || 500;
   console.log(statusCode === 500 ? err : err.message);
   res.status(statusCode).json({
-    error: {
-      message: err.message,
-    },
+    error: err.message,
   });
   return next();
 };
